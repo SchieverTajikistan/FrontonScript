36b765b9ff96dca360de9b0b7327649f39389e2c
Sukhrob Okhunov <okhunov.s@gmail.com>
2026-02-24 23:25:54 -0800

    RD15

--- other/activeScript.js
+++ other/activeScript.js
@@ -2,7 +2,7 @@
 //                                          //
 //     Modified : 2025-06-23 14:25 2025v6   //
 //                                          //
-//      Version : 6_1_15                     //
+//      Version : 6_1_16                     //
 //                                          //
 //       Author : RobotX, Kaliningrad, RU   //
 //                                          //
